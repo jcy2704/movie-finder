dbe7d92c5f8fb2897390db5d788b4ccc6c372f15
Steven J Chung <stevenjchung12@gmail.com>
2021-04-07 08:18:54 -0500

    Add fonts and scss

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
-import '../styles/App.css';
+import '../styles/app/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header" />
+      <header className="App-header">
+        adsfkl
+      </header>
     </div>
   );
 }
