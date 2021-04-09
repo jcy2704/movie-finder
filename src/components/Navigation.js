26b11ca4e9753ad5b8b61235edfe16dc8646aa95
Steven J Chung <stevenjchung12@gmail.com>
2021-04-08 19:26:33 -0500

    Add Navigation and styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
+import '../styles/navigation/Navigation.css';
 
 const Navigation = () => (
   <>
-    <Link to="/">Popular</Link>
-    <Link to="/top">Top Rated</Link>
-    <Link to="/upcoming">Upcoming</Link>
+    <div className="navigation-cont d-flex justify-content-between">
+      <div className="navigation">
+        <Link to="/">Popular</Link>
+        <Link to="/top">Top Rated</Link>
+        <Link to="/upcoming">Upcoming</Link>
+      </div>
+      <button type="button"><FontAwesomeIcon icon={faFilter}>Filter</FontAwesomeIcon></button>
+    </div>
   </>
 );
 
