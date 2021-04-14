4b8ae27ded0f3e88db5f02ec542bd25344740ad4
Steven J Chung <stevenjchung12@gmail.com>
2021-04-14 14:56:51 -0500

    Fix router issues and Add netlify redirects

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter, Route, Switch, Redirect,
+} from 'react-router-dom';
 import Trending from '../containers/Trending';
 import Popular from '../containers/Popular';
 import TopRated from '../containers/TopRated';
@@ -15,7 +17,10 @@ const Routes = () => (
       <Route path="/" component={Popular} exact />
       <Route path="/top" component={TopRated} />
       <Route path="/upcoming" component={Upcoming} />
-      <Route path="/movie/:id" component={Movie} />
+      <Route path="/movie/:id" component={Movie} exact />
+      <Route path="*">
+        <Redirect to="/" />
+      </Route>
     </Switch>
     <div id="modalsContainer" />
   </BrowserRouter>
